e0f39ceb3a9939a569c00d4a07403e5ebec12afb
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-06-21 16:42:48 -0300

    "Tratamento de exeção com handler"

---
+++ spring/01/jpa_leaning/aulaspring/src/main/java/com/aula/spring/data/jpa/aulaspring/handler/BusinessException.java
@@ -0,0 +1,12 @@
+package com.aula.spring.data.jpa.aulaspring.handler;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String mensagem) {
+        super(mensagem);
+    }
+
+    public BusinessException(String mensagem, Object ... params) {
+        super(String.format(mensagem, params));
+    }
+    
+}
--- spring/01/jpa_leaning/aulaspring/src/main/java/com/aula/spring/data/jpa/aulaspring/jpa/repository/UserRepository.java
+++ spring/01/jpa_leaning/aulaspring/src/main/java/com/aula/spring/data/jpa/aulaspring/jpa/repository/UserRepository.java
@@ -5,18 +5,24 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.aula.spring.data.jpa.aulaspring.handler.BusinessException;
+import com.aula.spring.data.jpa.aulaspring.handler.CampoObrigatorioException;
 import com.aula.spring.data.jpa.aulaspring.model.Usuario;
 
 /*Esta uma classe de mokagem para trabalhar com dados sem o uso do jpaRepository de fato */
 @Repository
 public class UserRepository {
-        public void save(Usuario usuario){
-        if(usuario.getId()==null)
-          System.out.println("SAVE - Recebendo o usuário na camada de repositório");
-        else
-         System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
-        
-        System.out.println(usuario);
+    public void save(Usuario usuario){
+        if(usuario.getLogin() == null) throw new CampoObrigatorioException("login");
+        else if(usuario.getPassword()==null) throw new BusinessException("password");
+        else{
+            if(usuario.getId()==null)
+            System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+            else
+            System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+            
+            System.out.println(usuario);
+        }
     }
     public void deleteById(Integer id){
         System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
