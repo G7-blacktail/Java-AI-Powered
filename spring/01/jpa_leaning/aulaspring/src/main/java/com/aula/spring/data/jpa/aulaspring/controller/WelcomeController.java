f6a33aba10b76a7a14c7d6cda9086c8da2121104
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2024-06-22 00:06:13 -0300

    "autorizações simples"

--- spring/01/jpa_leaning/aulaspring/src/main/java/com/aula/spring/data/jpa/aulaspring/controller/WelcomeController.java
+++ spring/01/jpa_leaning/aulaspring/src/main/java/com/aula/spring/data/jpa/aulaspring/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.aula.spring.data.jpa.aulaspring.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     
-    @GetMapping("/welcome")
+    @GetMapping
     public String welcome() {
         return "Bem vindo ao spring boot rest API";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
+    public String users() {
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasRole('MANAGERS')")
+    public String managers() {
+        return "Authorized manager";
     }
     
 }
